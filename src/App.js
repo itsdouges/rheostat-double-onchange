1df38dea7e5737d30ccf48bc6cd4703faf25bc4a
madou <laheen@gmail.com>
2018-01-18 09:22:05 +1100

    add input example

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    values: [20, 50]
+    values: [20, 50],
+    value: ""
   };
 
-  updateValues = ({ values }, context = "from rheostat") => {
-    console.log("i was updated!", context);
+  updateSlider = ({ values }, context = "from rheostat") => {
+    console.log("slider was updated!", context);
 
     this.setState({
       values: values
+    });
+  };
+
+  updateTextbox = (value, context = "from input") => {
+    console.log("textbox was updated!", context);
+
+    this.setState({
+      value
     });
   };
 
@@ -22,16 +31,30 @@ class App extends Component {
       <div className="App">
         <button
           style={{ marginBottom: "5em" }}
-          onClick={() => this.updateValues({ values: [10, 90] }, "from button")}
+          onClick={() => this.updateSlider({ values: [10, 90] }, "from button")}
         >
-          fix values
+          fix slider values
         </button>
 
         <Rheostat
-          onChange={this.updateValues}
+          onChange={this.updateSlider}
           max={100}
           values={this.state.values}
         />
+
+        <div>
+          <button
+            style={{ marginBottom: "5em" }}
+            onClick={() => this.updateTextbox("hello, world!", "from button")}
+          >
+            fix textbox value
+          </button>
+
+          <input
+            onChange={e => this.updateTextbox(e.target.value)}
+            value={this.state.value}
+          />
+        </div>
       </div>
     );
   }
